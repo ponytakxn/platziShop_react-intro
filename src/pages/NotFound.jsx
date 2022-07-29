8141d9426581e9840e32e4a396c6288356e6d783
ponytaKXN <ju.valdesv@duocuc.cl>
2022-07-28 20:50:29 -0400

    glitch 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,12 +4,21 @@ import '@styles/NotFound.scss';
 const NotFound = () => {
     return(
         <div className='glitch-container'>
-            <h1 class="glitch">
-                <span aria-hidden="true">404 Not found</span>
-                404 Not found
-                <span aria-hidden="true">404 Not found</span>
+            <h1 className="glitch appear">
+                <span aria-hidden="true">Error 404</span>
+                Error 404
+                <span aria-hidden="true">Error 404</span>
             </h1>
+            
+            <h1 className="glitch hide">
+                <span aria-hidden="true">Not found</span>
+                Not found
+                <span aria-hidden="true">Not found</span>
+            </h1>
+            
         </div>
+        
+    
     )
 }
 
